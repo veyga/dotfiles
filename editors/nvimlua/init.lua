e9c967b2d6d17276c3d5322198996fcaa7de9426
Andrew Stefanich <andrewstefanich@gmail.com>
2024-01-15 14:53:07 -0500

    initial init

---
+++ editors/nvimlua/init.lua
@@ -0,0 +1,27 @@
+vim.g.mapleader = ' '
+
+-- vim.opt used for vim.o vim.wo vim.bo namespaces
+vim.opt.splitbelow = true
+vim.opt.relativenumber = true
+vim.opt.colorcolumn = "89"
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {}
+local opts = {}
+require("lazy").setup(plugins, opts)
