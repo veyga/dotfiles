7cc8bfccbb6da78c0ec6b2f9c01265e3e8728576
Andrew Stefanich <andrewstefanich@gmail.com>
2024-01-15 19:10:18 -0500

    more nvim configs

---
+++ editors/nvimlua/lua/plugins/lsp-config.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "clangd",
+          "lua_ls",
+          "pyright",
+          "tsserver",
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.yamlls.setup({})
+      lspconfig.vimls.setup({})
+      lspconfig.terraformls.setup({})
+      lspconfig.jsonls.setup({})
+      lspconfig.helm_ls.setup({})
+      lspconfig.dockerls.setup({})
+      lspconfig.bashls.setup({})
+      lspconfig.clangd.setup({})
+      lspconfig.lua_ls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.tsserver.setup({})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  },
+}
--- editors/nvimlua/lua/plugins/telescope.lua
+++ editors/nvimlua/lua/plugins/telescope.lua
@@ -4,14 +4,20 @@ return  {
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+      require("telescope").setup({
+        defaults = {
+          layout_strategy = "vertical",
+          file_ignore_patterns = {
+            "node_modules/.*",
+            "yarn.lock",
+            "package%-lock.json",
+            "lazy%-lock.json",
+            ".git/.*",
+          },
+        },
+      })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>f", builtin.find_files, {})
       vim.keymap.set("n", "<leader>r", builtin.live_grep, {})
     end,
   }
-
-    -- opts = {
-    --   defaults = {
-    --     layout_strategy = "vertical",
-    --   }
-    -- },
